de5dcba99ddbaa1f530863f66dcd9420be4bbabf
palm <zhuyiran@codinglink.tech>
2020-12-14 12:18:15 +0800

    基本功能实现

---
+++ Client/src/main/java/tech/codinglink/ClientRun.java
@@ -0,0 +1,8 @@
+package tech.codinglink;
+
+public class ClientRun {
+    public static void main(String[] args) {
+        ClientFrame clientFrame=new ClientFrame();
+    }
+}
+
---
+++ Server/src/main/java/tech/codinglink/ServerRun.java
@@ -0,0 +1,7 @@
+package tech.codinglink;
+
+public class ServerRun {
+    public static void main(String[] args) {
+        ServerFrame serverFrame=new ServerFrame();
+    }
+}
