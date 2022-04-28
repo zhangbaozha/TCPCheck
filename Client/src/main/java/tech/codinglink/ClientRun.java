616445fa605820c37dd1cfcfbbd72ef6061b1ce7
zhw <643412856@qq.com>
2022-04-28 20:10:49 +0800

    第一次提交

--- Client/src/main/java/tech/codinglink/ClientRun.java
+++
@@ -1,8 +0,0 @@
-package tech.codinglink;
-
-public class ClientRun {
-    public static void main(String[] args) {
-        ClientFrame clientFrame=new ClientFrame();
-    }
-}
-
--- Server/src/main/java/tech/codinglink/ServerRun.java
+++
@@ -1,7 +0,0 @@
-package tech.codinglink;
-
-public class ServerRun {
-    public static void main(String[] args) {
-        ServerFrame serverFrame=new ServerFrame();
-    }
-}
